0a4dc360b4b01b625b94eca4c2be9ed0e63c4cc9
Phillip Weinberg <weinbe58@gmail.com>
2022-12-18 12:11:00 -0500

    adding on the fly eval + fixing auto const&

--- src/includes/basis/basis.h
+++ src/includes/basis/basis.h
@@ -27,32 +27,32 @@ public:
     template<typename T>
     void ref_states_conj(
         const typename index_t i,
-        std::unrdered_map<typename bitset_t,T> col_states, 
+        std::unrdered_map<typename bitset_t,T>& col_states, 
         std::unrdered_map<typename index_t,T>& columns
     ){
-        for(auto const& ele : col_states){
-            auto refstate = symmetry.get_refstate(ele.first);
-            typename index_t j = state.get_index(refstate.first);
-            typename norm_t norm_j = space.get_norm(j);
-            typename norm_t norm_i = space.get_norm(i);
-            
-            columns[state_index] = ele.second * std::conj(refstate.second) * std::sqrt(double(norm_j)/ norm_i);
+        for(const auto& [state,raw_mat_ele] : col_states){
+            const auto& [ref_state,charater] = symmetry.get_refstate(state);
+            const typename index_t j = space->get_index(ref_state);
+            const typename norm_t norm_j = space->get_norm(j);
+            typename norm_t norm_i = space->get_norm(i);
+            const T mat_ele =  raw_mat_ele * std::conj(charater) * std::sqrt(double(norm_j) / norm_i);
+            (columns.contains(state_index) ?  columns[state_index] = mat_ele : columns[state_index] += mat_ele);
         }
     }
 
     template<typename T>
     void ref_states(
         const typename index_t i,
-        std::unrdered_map<typename bitset_t,T> row_states, 
+        std::unrdered_map<typename bitset_t,T>& row_states, 
         std::unrdered_map<typename index_t,T>& rows    
     ){
-        for(auto const& ele : row_states){
-            auto refstate = symmetry.get_refstate(ele.first);
-            typename index_t j = state.get_index(refstate.first);
-            typename norm_t norm_j = space.get_norm(j);
-            typename norm_t norm_i = space.get_norm(i);
-            
-            rows[state_index] = ele.second * refstate.second * std::sqrt(double(norm_j)/ norm_i);
+        for(const auto& [state,raw_mat_ele] : row_states){
+            const auto& [ref_state,charater] = symmetry.get_refstate(state);
+            typename index_t j = space->get_index(ref_state);
+            typename norm_t norm_j = space->get_norm(j);
+            typename norm_t norm_i = space->get_norm(i);
+            const T mat_ele =  raw_mat_ele * charater * std::sqrt(double(norm_j) / norm_i);
+            (rows.contains(state_index) ? : rows[state_index] = mat_ele : rows[state_index] += mat_ele);
         }
     }
 
@@ -75,14 +75,14 @@ public:
             columns.clear();
             auto state = space.get_state(row);
             // generate action on states
-            for(auto const& pterm : pterms){
+            for(const auto& pterm : pterms){
                 pterm.op_dagger<typename bitset_t>(state,col_states);
             }
-            for(auto const& dterm : dterms){
+            for(const auto& dterm : dterms){
                 pterm.op_dagger<typename bitset_t>(state,col_states);
             }
             // calculate location of states in basis
-            this->ref_state_conj(col_states,columns);
+            this->ref_state_conj<T>(col_states,columns);
             // insert number of non-zeros elements for this row
             rowptr[row] = columns.size();
         }
@@ -122,14 +122,14 @@ public:
 
             auto state = space.get_state(row);
             // generate action on states
-            for(auto const& pterm : pterms){
+            for(const auto& pterm : pterms){
                 pterm.op_dagger<typename bitset_t>(state,col_states);
             }
-            for(auto const& dterm : dterms){
+            for(const auto& dterm : dterms){
                 pterm.op_dagger<typename bitset_t>(state,col_states);
             }
             // calculate location of states in basis
-            this->ref_state_conj(col_states,columns);
+            this->ref_state_conj<T>(col_states,columns);
 
             // sort columns
             sorted_columns.insert(columns.begin(), columns.end());
@@ -142,7 +142,7 @@ public:
 
             // insert data
             typename index_t i = rowptr[row];
-            for(auto const& [col,nzval] : sorted_columns){
+            for(const auto& [col,nzval] : sorted_columns){
                 indices[i] = col;
                 values[i++] = nzval;
             }
@@ -151,6 +151,49 @@ public:
 
     }
 
+
+    template<typename X,typename Y>
+    void on_the_fly(
+        std::vector<operator_string<T>>& pterms,
+        std::vector<dense_term<T>>& dterms,
+        const Y a,
+        const X * x, 
+        const Y b, 
+        Y  * y
+    ){
+
+        if(b == Y(0.0)){
+            std::fill(y,y+space->size(),0)
+        }
+        else{
+            for(typename index_t index=0;index < space-<size();++index){ y[index] *= b; }
+        }
+
+        std::unordered_map<typename bitset_t,T> row_states;
+        std::unordered_map<typename index_t,T> matrix_ele;
+
+        for(typename index_t row=0;row < space->size();++row){
+            row_states.clear();
+            matrix_ele.clear();
+
+            auto state = space.get_state(row);
+            // generate action on states
+            for(const auto& pterm : pterms){
+                pterm.op<typename bitset_t>(state,col_states);
+            }
+            for(const auto& dterm : dterms){
+                pterm.op<typename bitset_t>(state,col_states);
+            }
+            // calculate location of states in basis
+            this->ref_state<T>(row_states,matrix_ele);
+
+            Y total = 0;
+            for(const auto& [col,nzval] : matrix_ele){total += nzval * x[col];}
+            y[row] += a * total;
+
+            matrix_ele.clear();
+        }
+    }
 
 };
 
