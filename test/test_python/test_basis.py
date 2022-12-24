76518457da14e5d57c6a9f2e05e6991a49d69feb
Phillip Weinberg <weinbe58@gmail.com>
2022-12-23 23:54:10 -0500

    fixing test

--- test/test_python/test_basis.py
+++ test/test_python/test_basis.py
@@ -4,4 +4,4 @@ from quspin_core.basis import bit_basis
 class TestBasis(unittest.TestCase):
     def test(self):
         b = bit_basis()
-        self.assertEqual(b._name="basis")
+        self.assertEqual(b._name=="basis")
