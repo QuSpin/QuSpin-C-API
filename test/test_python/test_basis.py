a23af34feb286e99ac240bda5fc6f53b8e08bb40
Phillip Weinberg <weinbe58@gmail.com>
2022-12-23 23:53:03 -0500

    adding python tests

---
+++ test/test_python/test_basis.py
@@ -0,0 +1,7 @@
+import unittest
+from quspin_core.basis import bit_basis
+
+class TestBasis(unittest.TestCase):
+    def test(self):
+        b = bit_basis()
+        self.assertEqual(b._name="basis")
