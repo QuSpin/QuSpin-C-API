43e7835854e55d0d081d84e9f8d5bdca5038a70d
Phillip Weinberg <weinbe58@gmail.com>
2022-12-26 16:34:45 -0500

    fixing tests untracking cython files

--- setup.py
+++ setup.py
@@ -1,48 +1,42 @@
 from setuptools import find_packages, setup, Extension
 from Cython.Build import cythonize
 import numpy as np
-import os,sys
+import os
 
-def pwd():
-    return os.path.split(os.path.abspath(__file__))[0]
 
 def get_includes():
-    return [np.get_include(),
-            os.path.join(pwd(),"src","quspin_cpp_api"),
-            os.path.join(pwd(),"src","quspin_core","include")]
+    return [np.get_include(),os.path.join("src","quspin_core","includes")]
 
 if __name__ == "__main__":
 
     with open("README.md", 'r') as f:
         long_description = f.read()
-        
+    
+    exec(open(os.path.join("src","quspin_core","_version.py")).read())
     ext = [
         Extension("quspin_core.basis", [os.path.join("src","quspin_core","basis.pyx")],
             include_dirs=get_includes(),
-            language_level=3,
-            language="c++",
         ),
         Extension("quspin_core.operators", [os.path.join("src","quspin_core","operator.pyx")],
             include_dirs=get_includes(),
-            language_level=3,
-            language="c++",
         ),
     ]
     
     setup(
         name="quspin-core",
-        version="0.0.1",
+        version=__version__,
         zip_safe=False,
         packages=find_packages(where="src"),
         package_dir={"": "src"},
         author="Phillip Weinberg, Marin Bukov, Markus Schmitt",
         description="Base low-level components for QuSpin.",
         long_description=long_description,
-        long_description_content_type='text/markdown',
         url="https://github.com/weinbe58/QuSpin-Core",
-        ext_modules=cythonize(ext),
+        ext_modules=cythonize(ext,
+            include_path=get_includes()
+        ),
         install_requires=[
-            'numpy>=1.19.2',
+            "numpy>=1.19.2",
         ],
         include_dirs=get_includes(),
     )
---
+++ test/test_python/test_operator.py
@@ -0,0 +1,7 @@
+import unittest
+from quspin_core.operator import bit_basis
+
+class TestBasis(unittest.TestCase):
+    def test(self):
+        op = operator()
+        self.assertEqual(op.name=="basis")
