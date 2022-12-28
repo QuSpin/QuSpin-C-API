8752d7781c010fccf38fb3e2e3ce98570b9b6b86
weinbe58 <weinbe58@gmail.com>
2022-12-28 10:43:29 -0500

    overloading conj to utils

--- src/quspin_core/includes/quspin/utils/functions.h
+++ src/quspin_core/includes/quspin/utils/functions.h
@@ -7,24 +7,14 @@
 namespace quspin {
 
 
-template<typename T>
-T real_value(const T v){return v;}
+template<typename T> T real_value(const T v){return v;}
 
-template<typename T>
-T real_value(const std::complex<T>& v){return v.real();}
+template<typename T> T real_value(const std::complex<T>& v){return v.real();}
 
-}
+template<typename T> T conj(const T v){return v;}
 
+template<typename T> T conj(const std::complex<T>& v){return std::conj(v);}
 
-template<typename I>
-std::hash(quspin::basis::bit_set<I> &state){
-    return std::hash(state.content);
-}
-
-
-template<typename I>
-std::hash(quspin::basis::dit_set<I> &state){
-    return std::hash(state.content);
 }
 
 #endif
