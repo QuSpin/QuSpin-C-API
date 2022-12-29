b678e2c070a63a3de872f04c714ed6f1342bf618
Phillip Weinberg <weinbe58@gmail.com>
2022-12-29 18:45:49 -0500

    adding tempplate integer power

--- src/quspin_core/includes/quspin/utils/functions.h
+++ src/quspin_core/includes/quspin/utils/functions.h
@@ -15,6 +15,39 @@ template<typename T> T conj(const T v){return v;}
 
 template<typename T> T conj(const std::complex<T>& v){return std::conj(v);}
 
+
+
+template<int base,int N>
+struct integer_pow {
+    enum {value = base * integer_pow<base,N-1>::value};
+    
+};
+
+template<int base>
+struct integer_pow<base,1>{
+    enum {value = base};
+};
+
+template<int base>
+struct integer_pow<base,0>{
+    enum {value = 1};
+};
+
 }
+
+#ifdef QUSPIN_UNIT_TESTS
+
+
+TEST_CASE("integer_pow"){
+    using namespace quspin;
+
+    CHECK(integer_pow<2,4>::value == 16);
+    CHECK(integer_pow<3,3>::value == 27);
+    CHECK(integer_pow<10,3>::value == 1000);
+    CHECK(integer_pow<5,5>::value == 3125);
+    
+}
+
+#endif
 
 #endif
