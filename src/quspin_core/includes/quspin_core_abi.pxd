19c3446a999acdc0bc6ad3102609a57b2037a36a
Phillip Weinberg <weinbe58@gmail.com>
2023-01-15 16:25:12 -0500

    adding symmetry Cython API

--- src/quspin_core/includes/quspin_core_abi.pxd
+++ src/quspin_core/includes/quspin_core_abi.pxd
@@ -1,2 +1,33 @@
+from libcpp.vector cimport vector
+from libcpp.memory cimport shared_ptr
+from libcpp cimport bool
+from numpy cimport complex128_t
+
 cdef extern from 'quspin_core_abi/quspin_core_abi.h' namespace 'quspin_core_abi':
     pass
+
+cdef extern from 'quspin_core_abi/symmetry_abi.h' namespace 'quspin_core_abi':
+    ctypedef struct bit_perm_args:
+        vector[int] perm
+
+    ctypedef struct dit_perm_args:
+        vector[int] perm
+
+    ctypedef struct perm_bit_args:
+        vector[int] mask
+
+    ctypedef struct perm_dit_args:
+        vector[vector[int]] perm
+        vector[int] locs
+
+    cdef cppclass symmetry_abi:
+        symmetry_abi(
+            const int,
+            const size_t,
+            vector[shared_ptr[void]],
+            const complex128_t *,
+            vector[shared_ptr[void]],
+            const complex128_t *)  except +
+
+
+
--- src/quspin_core/symmetry.pyx
+++ src/quspin_core/symmetry.pyx
@@ -1,4 +1,66 @@
 # cython: language_level=3
 # distutils: language=c++
-from quspin_core_abi import *
+from quspin_core_abi cimport symmetry_abi,bit_perm_args,perm_bit_args,dit_perm_args,perm_dit_args
+from numpy cimport complex128_t
+from libcpp.vector cimport vector
+from libcpp.memory cimport shared_ptr,make_shared,reinterpret_pointer_cast
 
+
+cdef class symmetry_api:
+    """An Extension class that constructs a low-level QuSpin symmetry_abi object."""
+
+    cdef symmetry_abi * symm
+
+    def __cinit__(
+        self,
+        int lhss, 
+        int bits,
+        lat_args,
+        complex128_t[::1] lat_chars,
+        loc_args,
+        complex128_t[::1] loc_chars):
+        cdef vector[shared_ptr[void]] _lat_args
+        cdef vector[shared_ptr[void]] _loc_args
+
+        cdef vector[int] int_vec
+        cdef vector[vector[int]] int_vec_vec
+
+        if lhss == 2:
+            for perm in lat_args:
+                int_vec = perm
+                _lat_args.push_back(
+                    reinterpret_pointer_cast[void,bit_perm_args](
+                        make_shared[bit_perm_args](int_vec)
+                    )
+                )
+            
+            for mask in loc_args:
+                int_vec = mask
+                _loc_args.push_back(
+                    reinterpret_pointer_cast[void,perm_bit_args](
+                        make_shared[perm_bit_args](int_vec)
+                    )
+                )
+
+        elif lhss > 2:
+            for perm in lat_args:
+                int_vec = perm
+                _lat_args.push_back(
+                    reinterpret_pointer_cast[void,dit_perm_args](
+                        make_shared[dit_perm_args](int_vec)
+                    )
+                )
+
+            for perms,locs in loc_args:
+                int_vec = locs
+                int_vec_vec = perms
+                _loc_args.push_back(
+                    reinterpret_pointer_cast[void,perm_dit_args](
+                        make_shared[perm_dit_args](int_vec_vec,int_vec)
+                    )
+                )
+
+        self.symm = new symmetry_abi(lhss,bits,_lat_args,&lat_chars[0],_loc_args,&loc_chars[0])
+
+    def __dealloc__(self):
+        del self.symm
