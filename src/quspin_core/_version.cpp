7db13d7a7e9eae0a7f657bddb73371a46c958f26
Phillip Weinberg <weinbe58@gmail.com>
2024-10-07 10:15:21 -0400

    setting up package

---
+++ src/quspin_core/_version.cpp
@@ -0,0 +1,12 @@
+#include <pybind11/pybind11.h>
+
+#define QUOTE(name) #name
+#define STR(macro) QUOTE(macro)
+
+#define VERSION_STR STR(VERSION_INFO)
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_version, m) {
+  m.def("_get_version", []() { return VERSION_STR; });
+}
