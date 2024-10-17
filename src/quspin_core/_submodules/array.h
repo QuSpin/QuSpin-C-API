09ca426400d8ac228adcbd83bd00515741c41972
Phillip Weinberg <weinbe58@gmail.com>
2024-10-16 21:00:29 -0400

    adding functions to headers.

--- src/quspin_core/_submodules/array.h
+++ src/quspin_core/_submodules/array.h
@@ -4,4 +4,8 @@
 
 namespace py = pybind11;
 
+quspin::DType numpy_to_quspin_dtype(const py::dtype &);
+quspin::Array to_array(py::buffer &);
+const quspin::Array to_array(const py::buffer &);
+
 void define_array(py::module_ &);
