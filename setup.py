ea03dad599914df30f136864d817bbf70dc4661e
Phillip Weinberg <weinbe58@gmail.com>
2024-10-17 22:53:31 +0000

    fixing windows build.

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ def setup_quspin_core() -> Dict[str, List[str]]:
 
     if sys.platform == "win32":
         obj_ext = "obj"
-        lib_file = "quspin.dll"
+        lib_file = "quspin.lib"
     elif sys.platform == "darwin":
         obj_ext = "o"
         lib_file = "libquspin.dylib"
