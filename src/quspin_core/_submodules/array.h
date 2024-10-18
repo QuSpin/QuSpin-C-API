85c5b47f9c35ae74e0c066837e72e7835ee5ad8f
Phillip Weinberg <weinbe58@gmail.com>
2024-10-18 14:31:24 +0000

    simplofy bindings.

--- src/quspin_core/_submodules/array.h
+++ src/quspin_core/_submodules/array.h
@@ -6,6 +6,14 @@
 
 namespace py = pybind11;
 
+template <typename To, typename From>
+std::vector<To> cast_vector(const std::vector<From> &in) {
+  std::vector<To> &&vec = std::vector<To>(in.size());
+  std::transform(in.begin(), in.end(), vec.begin(),
+                 [](const From &val) { return static_cast<To>(val); });
+  return std::move(vec);
+}
+
 quspin::DType numpy_to_quspin_dtype(const py::dtype &);
 quspin::Array to_array(py::buffer &);
 const quspin::Array to_array(const py::buffer &);
